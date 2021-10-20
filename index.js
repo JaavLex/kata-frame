6c2abf045fb166789f45b0161fb419b785176a0e
JaavLex <alexandre.javet@hotmail.com>
2021-10-20 09:10:29 +0200

    [feat] SQUARE => RECTANGLE

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-function getFrame(square, circle) {
+function getFrame(rectangle, circle) {
   let xFrameValues = () => {
     let x1 = 0;
     let x2 = 0;
-    if (square.x > x1) x1 = square.x;
+    if (rectangle.x > x1) x1 = rectangle.x;
     if (circle.x - circle.r < x1) x1 = circle.x - circle.r;
-    if (square.x + square.w > x2) x2 = square.x + square.w;
+    if (rectangle.x + rectangle.w > x2) x2 = rectangle.x + rectangle.w;
     if (circle.x + circle.r > x2) x2 = circle.x + circle.r;
     return { x1: x1, x2: x2 };
   }
@@ -15,9 +15,9 @@ function getFrame(square, circle) {
   let yFrameValues = () => {
     let y1 = 0;
     let y2 = 0;
-    if (square.y > y1) y1 = square.y;
+    if (rectangle.y > y1) y1 = rectangle.y;
     if (circle.y - circle.r < y1) y1 = circle.y - circle.r;
-    if (square.y + square.h > y2) y2 = square.y + square.h;
+    if (rectangle.y + rectangle.h > y2) y2 = rectangle.y + rectangle.h;
     if (circle.y + circle.r > y2) y2 = circle.y + circle.r;
     return { y1: y1, y2: y2 };
   }
@@ -25,11 +25,11 @@ function getFrame(square, circle) {
 }
 
 function drawShapes() {
-  let square = {
-    x: parseInt(document.getElementById("sqX").value, 10) || 125,
-    y: parseInt(document.getElementById("sqY").value, 10) || 125,
-    w: parseInt(document.getElementById("sqW").value, 10) || 50,
-    h: parseInt(document.getElementById("sqH").value, 10) || 50
+  let rectangle = {
+    x: parseInt(document.getElementById("reX").value, 10) || 125,
+    y: parseInt(document.getElementById("reY").value, 10) || 125,
+    w: parseInt(document.getElementById("reW").value, 10) || 50,
+    h: parseInt(document.getElementById("reH").value, 10) || 50
   }
 
   let circle = {
@@ -39,13 +39,13 @@ function drawShapes() {
   }
 
   let frame = {
-    x: getFrame(square, circle).xFVal.x1,
-    y: getFrame(square, circle).yFVal.y1,
-    w: getFrame(square, circle).xFVal.x2 - getFrame(square, circle).xFVal.x1,
-    h: getFrame(square, circle).yFVal.y2 - getFrame(square, circle).yFVal.y1
+    x: getFrame(rectangle, circle).xFVal.x1,
+    y: getFrame(rectangle, circle).yFVal.y1,
+    w: getFrame(rectangle, circle).xFVal.x2 - getFrame(rectangle, circle).xFVal.x1,
+    h: getFrame(rectangle, circle).yFVal.y2 - getFrame(rectangle, circle).yFVal.y1
   }
 
-  if (getFrame(square, circle).xFVal.x1 < 0 || getFrame(square, circle).xFVal.x2 > 500 || getFrame(square, circle).yFVal.y1 < 0 || getFrame(square, circle).yFVal.y2 > 500) {
+  if (getFrame(rectangle, circle).xFVal.x1 < 0 || getFrame(rectangle, circle).xFVal.x2 > 500 || getFrame(rectangle, circle).yFVal.y1 < 0 || getFrame(rectangle, circle).yFVal.y2 > 500) {
     document.getElementById("alerting").innerHTML = "INVALID";
     document.getElementById("canvas").style = "border: 1px solid; border-color: red";
   } else {
@@ -54,11 +54,11 @@ function drawShapes() {
   }
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  console.log(`SQUARE - x: ${square.x} | y: ${square.y} | w: ${square.w} | h: ${square.h}`)
+  console.log(`rectangle - x: ${rectangle.x} | y: ${rectangle.y} | w: ${rectangle.w} | h: ${rectangle.h}`)
   console.log(`CIRCLE - x: ${circle.x} | y: ${circle.y} | r: ${circle.r}`)
   console.log(`FRAME - x: ${frame.x} | y: ${frame.y} | w: ${frame.w} | h: ${frame.h}`)
   ctx.fillStyle = 'blue';
-  ctx.fillRect(square.x, square.y, square.w, square.h);
+  ctx.fillRect(rectangle.x, rectangle.y, rectangle.w, rectangle.h);
   ctx.fillStyle = 'red';
   ctx.beginPath();
   ctx.ellipse(circle.x, circle.y, circle.r, circle.r, 0, 0, 2 * Math.PI);
@@ -68,10 +68,10 @@ function drawShapes() {
 }
 
 function randomVals() {
-  let sqXCoord = Math.floor(Math.random() * 450);
-  let sqYCoord = Math.floor(Math.random() * 450);
-  let sqWidth = Math.floor(Math.random() * (500 - sqXCoord))
-  let sqHeight = Math.floor(Math.random() * (500 - sqYCoord))
+  let reXCoord = Math.floor(Math.random() * 450);
+  let reYCoord = Math.floor(Math.random() * 450);
+  let reWidth = Math.floor(Math.random() * (500 - reXCoord))
+  let reHeight = Math.floor(Math.random() * (500 - reYCoord))
   let ciXCoord = Math.floor(Math.random() * 450);
   let ciYCoord = Math.floor(Math.random() * 450);
   let ciRadius = 1000;
@@ -80,10 +80,10 @@ function randomVals() {
     ciRadius = Math.floor(Math.random() * 450) + 1;
   }
 
-  document.getElementById("sqX").value = sqXCoord;
-  document.getElementById("sqY").value = sqYCoord;
-  document.getElementById("sqW").value = sqWidth;
-  document.getElementById("sqH").value = sqHeight;
+  document.getElementById("reX").value = reXCoord;
+  document.getElementById("reY").value = reYCoord;
+  document.getElementById("reW").value = reWidth;
+  document.getElementById("reH").value = reHeight;
   document.getElementById("ciX").value = ciXCoord;
   document.getElementById("ciY").value = ciYCoord;
   document.getElementById("ciR").value = ciRadius;
